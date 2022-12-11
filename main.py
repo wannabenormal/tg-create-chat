053832264c8b1407d4e9838a2fef82a6df13b6a9
Alexey Pesotsky <pesthedog@mail.ru>
2022-12-11 15:57:17 +0400

    Add encryption key to telegram class

--- main.py
+++ main.py
@@ -8,11 +8,13 @@ if __name__ == '__main__':
     tg_api_id = env.str('TG_API_ID')
     tg_api_hash = env.str('TG_API_HASH')
     tg_phone = env.str('TG_PHONE')
+    db_encryption_key = env.str('DB_ENCRYPTION_KEY')
 
     tg = Telegram(
         api_id=tg_api_id,
         api_hash=tg_api_hash,
-        phone=tg_phone
+        phone=tg_phone,
+        database_encryption_key=db_encryption_key
     )
 
     tg.login()
