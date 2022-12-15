344c353c524a57ba391b6378cc92adbb110f68aa
Alexey Pesotsky <pesthedog@mail.ru>
2022-12-15 13:25:02 +0400

    Ad ifmain construction

--- create_tg_chat.py
+++ create_tg_chat.py
@@ -1,18 +1,42 @@
+import json
+import argparse
+import time
+
+from environs import Env
 from telegram.client import Telegram
 
 
 def create_tg_chat(
-    api_id,
-    api_hash,
-    phone,
-    db_encryption_key,
+    tg,
     chat_title='Название чата',
     user_ids=[]
 ):
+    params = {
+        'title': chat_title,
+        'user_ids': user_ids,
+    }
+
+    tg.call_method('createNewBasicGroupChat', params=params, block=True)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Function to create telegram chat'
+    )
+    parser.add_argument('path', type='str', help='Path to JSON')
+    args = parser.parse_args()
+
+    env = Env()
+    env.read_env()
+    tg_api_id = env.str('TG_API_ID')
+    tg_api_hash = env.str('TG_API_HASH')
+    tg_phone = env.str('TG_API_HASH')
+    db_encryption_key = env.str('DB_ENCRYPTION_KEY')
+
     tg = Telegram(
-        api_id=api_id,
-        api_hash=api_hash,
-        phone=phone,
+        api_id=tg_api_id,
+        api_hash=tg_api_hash,
+        phone=tg_phone,
         database_encryption_key=db_encryption_key
     )
 
@@ -21,11 +45,9 @@ def create_tg_chat(
     result = tg.get_chats()
     result.wait()
 
-    params = {
-        'title': chat_title,
-        'user_ids': user_ids,
-    }
+    with open(args.path, 'r') as file:
+        groups = json.load(file)
 
-    tg.call_method('createNewBasicGroupChat', params=params, block=True)
-
-    tg.stop()
+    for index, group in enumerate(groups, start=1):
+        create_tg_chat(tg, f'Тестовая группа {index}', group)
+        time.sleep(1)
